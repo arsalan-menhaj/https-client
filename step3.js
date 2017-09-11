841bce8ed16ac5dab1d9ad439745de6d7acea639
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-11 15:35:41 +0000

    Modified getAndPrintHTML function to accept a parameter

---
+++ step3.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  var output = '';
+
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data.length);
+      output += data;
+    });
+
+    response.on('end', function() {
+      console.log(output);
+      console.log('Response stream complete.');
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
